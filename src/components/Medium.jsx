16960cbe57cb58b2c094c5eab4a2f8e4aa340aa8
Bill-1991 <altzebill@yahoo.com>
2024-07-11 12:24:19 +0300

    change render logic

--- src/components/Medium.jsx
+++ src/components/Medium.jsx
@@ -124,7 +124,9 @@ export default function Medium({ qrParam }) {
     //const name = "https://" + params.get("next")
     if (sitesLoading || vCardsLoading) return <h1>Loading...</h1>
     return(
-        object[0] ?
+        object[0].urlName ? <div></div>
+        :
+        object[0].firstName ?
         <VcardUi curVCard={object[0]} firstName={object[0].firstName} lastName={object[0].lastName} title={object[0].title} email={object[0].email} 
         addressChange={addressChange} tel={object[0].phone} address={object[0].address} notes={object[0].notes} contactUrl={object[0].website} photo={object[0].photo} 
         fileName={fileName} fileNameChange={fileNameChange} downloadVcard={downloadVcard} />
