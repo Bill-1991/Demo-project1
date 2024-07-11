3b7ecf89fd9491381b71d4efb7d0bf5e8ea58cff
Bill-1991 <altzebill@yahoo.com>
2024-07-11 12:52:11 +0300

    fix code

--- src/components/Medium.jsx
+++ src/components/Medium.jsx
@@ -124,17 +124,14 @@ export default function Medium({ qrParam }) {
     //const name = "https://" + params.get("next")
     if (sitesLoading || vCardsLoading) return <h1>Loading...</h1>
     return(
-        object[0].urlName ? <div></div>
+        object[0].urlName && !object[0].firstName ? <div></div>
         :
-        object[0].firstName ?
+        object[0].firstName && object[0].urlName ?
         <VcardUi curVCard={object[0]} firstName={object[0].firstName} lastName={object[0].lastName} title={object[0].title} email={object[0].email} 
         addressChange={addressChange} tel={object[0].phone} address={object[0].address} notes={object[0].notes} contactUrl={object[0].website} photo={object[0].photo} 
         fileName={fileName} fileNameChange={fileNameChange} downloadVcard={downloadVcard} />
         :
-        !object[0].urlName && !object[0].firstName ?
         <h1>Not Found</h1>
-        :
-        undefined
     )
 }
 
