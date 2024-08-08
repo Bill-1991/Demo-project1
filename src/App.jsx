c1fa5903c41d0b5f809311adc99bf3a26712d76f
unknown <altzebill@yahoo.com>
2024-08-08 14:10:48 +0300

    debug deploy

--- src/App.jsx
+++ src/App.jsx
@@ -125,8 +125,8 @@ export default function App() {
   const [tel, setTel] = useState("")
   const [rawAddress, setRawAddress] = useState("")
   const [backRoute, setBackRoute] = useState("sites")
-  let siteUrl = `https://demo-project1-9big.onrender.com/${shortUrl}`
-  let vCardUrl = `https://demo-project1-9big.onrender.com/${shortUrl}`
+  let siteUrl = `https://demo-project1-lhwe.onrender.com/${shortUrl}`
+  let vCardUrl = `https://demo-project1-lhwe.onrender.com/${shortUrl}`
   let ref = useRef(null);
   
   //vcards?id=${vCardId}&preview=${preview}
