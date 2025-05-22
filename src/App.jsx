cea2a295df0c6abcdd71da66b41aed4e27bbf307
unknown <altzebill@yahoo.com>
2025-05-22 13:53:29 +0300

    vCard error

--- src/App.jsx
+++ src/App.jsx
@@ -321,11 +321,12 @@ export default function App() {
   }
 
   const onVCardsChange = () => {
+    console.log(backRoute)
     if (vCards === false) {
       setUrls(false)
       setVCards(true)
       setBackRoute("vcards")
-      console.log(backRoute)
+      
     }
   }
   
@@ -574,7 +575,7 @@ export default function App() {
       let qrBlob = await qrText.then(value => value.text().then(value => value));
       console.log(backRoute, "good")
       if (backRoute === "sites") {
-        Axios.post(`https://localhost:3001/${backRoute}/`, {
+        Axios.post(`http://localhost:3001/${backRoute}/`, {
           qrSvg: qrBlob,
           short: shortUrl,
           urlName: url
@@ -591,7 +592,7 @@ export default function App() {
       }
       //demo-project1-ms77.onrender.com
       else if (backRoute === "vcards") {
-        Axios.post(`https://localhost:3001/${backRoute}/`, {
+        Axios.post(`http://localhost:3001/${backRoute}/`, {
           qrSvg: qrBlob,
           short: shortUrl,
           firstName: firstName,
